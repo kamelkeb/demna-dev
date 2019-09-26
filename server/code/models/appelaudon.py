f28d61de0efca81d44b6445e0d544a104cb09c36
s4m75 <contact8395@laposte.net>
2019-09-26 17:28:26 +0200

    ajout modele appelaudon

---
+++ server/code/models/appelaudon.py
@@ -0,0 +1,58 @@
+from db import db
+
+
+class Appelaudon(db.Model):
+    __tablename__ = 'appelaudon'
+
+    idappelaudon = db.Column(db.Integer, primary_key=True)
+    fkidutilisateur = db.Column(db.Integer, db.ForeignKey(u'utilisateur.idutilisateur'), nullable=False)
+    caracterisationbiologique = db.Column(JSON, nullable=False)
+    quantiteattendue = db.Column(db.Integer, nullable=False)
+    lieudecollecte = db.Column(JSON, nullable=False)
+    calendriercollecte = db.Column(JSON, nullable=False)
+    motifetdescription = db.Column(db.String(1024), server_default=NULL)
+    priorite = db.Column(db.Integer, nullable=False)
+    contactsubst = db.Column(JSON, server_default=NULL)
+    statut = db.Column(db.Integer, nullable=False)
+    datecreation = db.Column(db.DateTime, nullable=False)
+    datemodification = db.Column(db.DateTime, nullable=False)
+
+    utilisateur = db.relationship(u'Utilisateur')
+
+    def __init__(self, idutilisateur, caracterisationbiologique, quantiteattendue, lieudecollecte, calendriercollecte, motifetdescription, priorite, contactsubst, statut, datecreation, datemodification):
+        self.idutilisateur = idutilisateur
+        self.caracterisationbiologique = caracterisationbiologique
+        self.quantiteattendue = quantiteattendue
+        self.lieudecollecte = lieudecollecte
+        self.calendriercollecte = calendriercollecte
+        self.motifetdescription = motifetdescription
+        self.priorite = priorite
+        self.contactsubst = contactsubst
+        self.statut = statut
+        self.datecreation = datecreation
+        self.datemodification = datemodification
+
+    def json(self):
+        return {'idutilisateur': self.idutilisateur,
+                'caracterisationbiologique': self.caracterisationbiologique,
+                'quantiteattendue': self.quantiteattendue,
+                'lieudecollecte': self.lieudecollecte,
+                'calendriercollecte': self.calendriercollecte,
+                'motifetdescription': self.motifetdescription,
+                'priorite': self.priorite,
+                'contactsubst': self.contactsubst,
+                'statut': self.statut,
+                'datecreation': self.datecreation,
+                'datemodification': self.datemodification}
+
+    @classmethod
+    def find_by_id(cls, _id):
+        return cls.query.filter_by(idappelaudon=_id).first()
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
